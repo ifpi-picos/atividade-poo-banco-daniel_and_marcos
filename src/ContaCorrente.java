6340d4c0fc436264451827140f06165fab6ecc01
DanielReisdC <49843024+DanielReisdC@users.noreply.github.com>
2022-11-04 22:04:22 -0300

    Update ContaCorrente.java

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,12 +1,18 @@
 public class ContaCorrente extends Conta {
     private double chequeEspecial;
+    
+    private int quantiTransf;
 
-    public ContaCorrente(int numAgencia, int numConta, double saldo) {
-        super(numAgencia, numConta, saldo);
+    public ContaCorrente(int numAgencia, int numConta, double saldo, Cliente cliente, Notificacao notificacao) {
+        super(numAgencia, numConta, notificacao, saldo, cliente);
+        this.chequeEspecial=100;
     }
     
     public double getChequeEspecial() {
         return chequeEspecial;
+    }
+    public void setChequeEspecial(double chequeEspecial){
+        this.chequeEspecial = chequeEspecial;
     }
 
     @Override
@@ -19,11 +25,27 @@ public class ContaCorrente extends Conta {
 		return super.depositar(valor);
 	}
     @Override
-    public void transferir(double saldo, Conta destinatario){
-		this.saldo+= saldo;
-		destinatario.saldo+=saldo;
+    public void transferir(Conta destinatario, double valor){
+        if(quantiTransf<2){
+        	saldo-=valor;
+        	destinatario.depositar(valor);
+        	enviaNotificacao("transferência", valor);
+        	quantiTransf++;
+        	System.out.println("Transferência efetuada com sucesso");
+        
+        }
+        else{
+        
+        	double taxa = 100*0.05;
+	        saldo -= (valor+ taxa);
+	        destinatario.depositar(valor);
+	        enviaNotificacao("Transferência", valor);
+	        System.out.println("Transferência efetuada com sucesso");
+        }
              
     }
-}
 
-    
+
+	
+   
+}
