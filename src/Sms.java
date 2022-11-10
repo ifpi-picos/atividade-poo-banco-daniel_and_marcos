f9321d283ca09c1633be37e187889a77459e9214
DanielReisdC <49843024+DanielReisdC@users.noreply.github.com>
2022-11-10 19:44:09 -0300

    Add files via upload

--- src/Sms.java
+++ src/Sms.java
@@ -3,7 +3,7 @@ public class Sms implements Notificacao {
 	
 	@Override
 	public void enviaNotificacao(String operacao, double valor){
-		
-	}
+		System.out.println("(Sms) operação concluida: " + operacao + valor);
 
+}
 }
